c0d3e77bcd65bffbacc576e68cac864e7d4cdf6d
jacksina <muminamutuku>
2022-11-08 21:51:41 +0300

    both forsale and forrent working

--- src/components/listproperty.component.jsx/EditProperty.jsx
+++ src/components/listproperty.component.jsx/EditProperty.jsx
@@ -57,9 +57,10 @@ const EditProperty = ({ currentUser, match: { params }}) => {
                                     </Select>
                                 </VStack>
                                 <DataContext.Provider value={{currentUser,property}}>
-                                    {/* {selectedPurpose === 'rent' ? */}
-                                        <ListpropertyForRent />
-                                    
+                                    {selectedPurpose === 'rent' ?
+                                        <ListpropertyForRent />:
+                                        <ListpropertyForsale />
+                                    }
                                 </DataContext.Provider>
                                 {/* <Amenities/> */}
                             </div>
@@ -580,9 +581,6 @@ function ListpropertyForRent() {
 
     const amenitiesList = "Parking, " +
         "Indoor game, 24x7 Sec, Balcony";
-
-    // const currentUser = React.useContext(OwnerContext)
-    // console.log(currentUser);
   
     let handleSubmit = async (e) => {
       e.preventDefault();
@@ -794,6 +792,227 @@ function ListpropertyForRent() {
                 </VStack>
             </div>
         </div>   
+    );
+}
+
+function ListpropertyForsale() {
+    const {currentUser, property} = useContext(DataContext);
+
+    const [price, setPrice] = useState(0);
+    const [location,setlocation] = useState("");
+    const [description, setdescription] = useState("");
+    const [message, setMessage] = useState("");
+    const [selectedbathroom, setSelectedbathroom] = useState(0);
+    const [selectedrooms, setSelectedrooms] = useState(0);
+    const [selectedfurnishingStatus, setSelectedfurnishingStatus] = useState(0);
+    const [selectedHouseType, setSelectedHouseType] = useState('');
+    const [coverPhoto, setCoverPhoto] = useState(null);
+    const [otherPhotos, setOtherPhotos] = useState(null);
+    // const [contactId, setContactId] = useState(0);
+
+    const [finalAmenitiesList, setFinalAmenitiesList] = useState([])
+    // console.log(finalAmenitiesList)
+
+    const amenitiesList = "Parking" +
+        "Indoor game, 24x7 Sec, Balcony";
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        let form_data = new FormData();
+        form_data.append('price',price)
+        form_data.append('location',location)
+        form_data.append('description', description)
+        form_data.append('bathrooms', selectedbathroom)
+        form_data.append('rooms', selectedrooms)
+        form_data.append('furnishingStatus', selectedfurnishingStatus)
+        form_data.append('purpose', 'For-Sale')
+        form_data.append('houseType', selectedHouseType)
+        form_data.append('amenities', finalAmenitiesList.join(","))
+        form_data.append('coverPhoto', coverPhoto)
+        form_data.append('otherPhotos', otherPhotos)
+  
+        if (!currentUser) {
+          throw new Error("You must be logged in to post a Property!");
+        }
+  
+      try {
+        const res = await axios.post('http://127.0.0.1:8000/forsale/', form_data, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+  
+        if (res.status === 200) {
+          setPrice(0);
+          setlocation("");
+          setdescription("");
+          setSelectedbathroom(0);
+          setSelectedrooms(0);
+          setSelectedfurnishingStatus(""); 
+          setSelectedHouseType("");
+          setMessage("User created successfully");
+        } else {
+          setMessage("Some error occured");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    let housetypeOptions = filterData.find(obj => {
+        return obj.queryName === 'housetype';
+    }).items;
+
+    let roomsOptions = filterData.find(obj => {
+        return obj.queryName === 'rooms';
+    }).items;
+
+    let bathroomOptions = filterData.find(obj => {
+        return obj.queryName === 'bathrooms';
+    }).items;
+
+    let furnishingStatusOptions = filterData.find(obj => {
+        return obj.queryName === 'furnishingStatus';
+    }).items;
+  
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="row pd-top-100 mb-4">
+                <div className="col-md-4">
+                    <div className="section-title"><h4><Image src="/assests/square-check.svg" alt="img"/>House Name</h4></div>
+                </div>
+                <div className="col-md-8">
+                    <div className="section-title"><Heading size='sm'>Jason Landville Apartments</Heading><p>Lorem ipsum dolor sit amet, consectetur adipiscing </p></div>
+                            <Stack spacing={4}>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents='none'
+                                        color='gray.300'
+                                        fontSize='1.2em'
+                                        children='$'
+                                    />
+                                        <Input
+                                            placeholder='Enter amount' 
+                                            value={price}
+                                            onChange={(e) => setPrice(e.target.value)}
+                                        />
+                                    <InputRightElement children={<BsCheckCircle  color='green.500' />} />
+                                </InputGroup>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents='none'
+                                        color='gray.300'
+                                        fontSize='1.2em'
+                                        children='$'
+                                    />
+                                        <Input 
+                                            placeholder='Enter Property Location'
+                                            value={location}
+                                            onChange={(e) => setlocation(e.target.value)}
+                                        />
+                                    <InputRightElement children={<BsCheckCircle  color='green.500' />} />
+                                </InputGroup>
+                            </Stack>
+                            <Stack direction="row" mt='5' mb='5'>
+                                <Select 
+                                    variant='filled' 
+                                    placeholder='houseType' 
+                                    size='md'
+                                    value={selectedHouseType}
+                                    onChange={(e) => setSelectedHouseType(e.target.value)}
+                                >
+                                    {housetypeOptions.map(option => {
+                                        return (<option key={option.value} value={option.value}>{option.name}</option>)
+                                    })}
+                                </Select>
+                                <Select 
+                                    variant='filled' 
+                                    placeholder='Bedrooms' 
+                                    size='md'
+                                    value={selectedrooms}
+                                    onChange={(e) => setSelectedrooms(e.target.value)}
+                                >
+                                    {roomsOptions.map(option => {
+                                        return (<option key={option.value} value={option.value}>{option.name}</option>)
+                                    })}
+                                </Select>
+                            </Stack>
+                            <Stack direction='row' mt='5' mb='5'>
+                                <Select 
+                                    variant='filled' 
+                                    placeholder='furnishingStatus' 
+                                    size='md'
+                                    value={selectedfurnishingStatus}
+                                    onChange={(e) => setSelectedfurnishingStatus(e.target.value)}
+                                >
+                                    {furnishingStatusOptions.map(option => {
+                                        return (<option key={option.value} value={option.value}>{option.name}</option>)
+                                    })}
+                                </Select>
+                                <Select 
+                                    variant='filled' 
+                                    placeholder='bathroom' 
+                                    size='md'
+                                    value={selectedbathroom}
+                                    onChange={(e) => setSelectedbathroom(e.target.value)}
+                                >
+                                    {bathroomOptions.map(option => {
+                                        return (<option key={option.value} value={option.value}>{option.name}</option>)
+                                    })}
+                                </Select>
+                            </Stack>
+                            <Textarea 
+                                placeholder='Enter a brief description of your property' 
+                                value={description}
+                                onChange={(e) => setdescription(e.target.value)}
+                            />
+                            <Stack direction='column'justifyContent='space-between' mt='5' mb='5'>
+                            <input 
+                                type='file'
+                                onChange={(e) => setCoverPhoto(e.target.files[0])}
+                                ></input>
+                            <input 
+                                type='file' 
+                                onChange={(e) => setOtherPhotos(e.target.files[0])}
+                                ></input>
+                            </Stack>
+                </div>
+            
+            </div>
+            <div className="row pd-top-80">
+                <div className="col-md-4">
+                    <div className="section-title"><h4><Image src="/assests/square-check.svg" alt="img"/>Amenities</h4></div>
+                </div>
+                <div className="col-md-8">
+                    <div className="section-title"><Heading size='sm'>Add The Available Amenities Here</Heading><p>Lorem ipsum dolor sit amet, consectetur adipiscing </p></div>
+                    <div className="row">
+                        {/*  TODO - fix final amenity list when unchecked */}
+                        {
+                            amenitiesList.split(",").map((amenity, index) => {
+                                amenity = amenity.trim();
+
+                                return (
+                                    <Checkbox 
+                                        key={index}
+                                        checked={true} 
+                                        onChange={
+                                            () => {
+                                                setFinalAmenitiesList([...finalAmenitiesList, amenity ])
+                                            }
+                                        } 
+                                        value={amenity} size='lg' colorScheme='orange'
+                                        >{amenity}
+                                    </Checkbox> 
+                                )
+                            })
+                        }
+                        <div className="col-12 mt-5">
+                            <Button type="submit" colorScheme='yellow'>Publish</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>   
     );
 }
 
